3361df79693cd95c4d3495f33fddfec296bcf5d8
vanely <vanelys94@gmail.com>
2022-01-24 23:17:18 -0500

    objects, and associative array updates

---
+++ 13-associative-arrays.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+# associative arrays in bash
+# https://linuxhint.com/associative_array_bash/
--- 14-objects.sh
+++ 14-objects.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# objects in bash
+# object emulation in bash
 # https://stackoverflow.com/questions/36771080/creating-classes-and-objects-using-bash-scripting/36771175
+# https://github.com/mnorin/bash-scripts/tree/master/objects
